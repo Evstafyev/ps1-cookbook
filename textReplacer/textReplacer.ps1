13ad1ae28a32f77db277d0bc73bea8977468a251
Aleksei Evstafev <aleksey.evstafiev@main.russianpost.ru>
2019-10-08 16:03:31 +0300

    Version 1.0

---
+++ textReplacer/textReplacer.ps1
@@ -0,0 +1,6 @@
+﻿#
+# Quick replace server IP in zabbix agent configuration file.
+#
+$path = "C:\zabbix\zabbix_agentd.conf"
+$inputFile = Get-Content $path
+$inputFile.Replace("192.168.1.10","192.168.1.5") | sc C:\zabbix\zabbix_agentd.conf
