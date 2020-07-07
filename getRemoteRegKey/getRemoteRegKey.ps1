ba98ea126685f9c585f6d90b4e146a0da6ab16d3
Aleksei Evstafev <a.a.evstafyev@gmail.com>
2020-07-07 11:15:42 +0300

    Version 2.0

---
+++ getRemoteRegKey/getRemoteRegKey.ps1
@@ -0,0 +1,16 @@
+﻿$HKLM = 2147483650
+
+$server = 'HOST1'
+
+$user = Read-Host "Enter domain\username"
+
+$pass = Read-Host -AsSecureString
+
+$userCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user,$pass
+
+$objReg = Get-WmiObject -List -Namespace root\default -ComputerName $server -Credential $userCredentials | 
+Where-Object{$_.Name -eq "StdRegProv"}
+
+$subKeyNames = $objReg.EnumValues($HKLM,"SYSTEM\ControlSet001\Control\Keyboard Layouts").sNames
+
+Write-host $subKeyNames
