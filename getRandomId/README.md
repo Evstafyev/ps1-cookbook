bb2e1f43db0282797e4fe3c87a4e8634577e07cb
Aleksei Evstafev <a.a.evstafyev@gmail.com>
2020-07-07 11:11:02 +0300

    Version 2.0

---
+++ getRandomId/README.md
@@ -0,0 +1,1 @@
+Random letter indentifier generator.
--- getRandomId/getRandomId.ps1
+++ getRandomId/getRandomId.ps1
@@ -2,4 +2,4 @@
 
 $str = 'ID'+ $(-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})).ToString().ToUpper() + "$(1..100 | Get-Random)"
 
-$str
+Write-host $str
